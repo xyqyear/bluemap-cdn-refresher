63cf9fe109fe7f108c9e490200b64d96c05a42da
xyqyear <xyqyear@gmail.com>
2023-08-09 15:14:53 +0800

    add debug message

--- bluemap_cdn_refresher/utils.py
+++ bluemap_cdn_refresher/utils.py
@@ -1,13 +1,20 @@
 import hashlib
 import json
+import time
+import logging
 
 from .file import get_data
 
 
 def compute_sha256(file_path):
     if file_path.endswith(".json.gz"):
+        before = time.perf_counter()
         json_data = get_data(file_path)
-        return hashlib.sha256(json.dumps(json_data).encode()).hexdigest()
+        then = time.perf_counter()
+        logging.debug(f"read data: {then - before}")
+        sha256 = hashlib.sha256(json.dumps(json_data).encode()).hexdigest()
+        logging.debug(f"sha256: {time.perf_counter() - then}")
+        return sha256
     else:
         h = hashlib.sha256()
         with open(file_path, "rb") as file:
