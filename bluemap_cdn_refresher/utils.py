cbe5a936af9a462494d17599d7b4af4356dff8d4
xyqyear <xyqyear@gmail.com>
2023-08-09 17:53:16 +0800

    welp

--- bluemap_cdn_refresher/utils.py
+++ bluemap_cdn_refresher/utils.py
@@ -9,7 +9,7 @@ def compute_xxh32(file_path):
         key_string = raw_json[88:92]
         if key_string != b'data':
             raise ValueError(f"the data format changed for bluemap")
-        xxh32 = xxhash.xxh32(key_string[93:]).intdigest()
+        xxh32 = xxhash.xxh32(raw_json[93:]).intdigest()
         return xxh32
     else:
         file_content = open(file_path, "rb").read()
